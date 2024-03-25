db322b1238163e32f74978496c3b96d7510dc179
Migas77 <miguel.belchior@ua.pt>
2024-03-25 17:43:48 +0000

    decoder service

---
+++ DecoderService/decode_token.py
@@ -0,0 +1,22 @@
+from fastapi import Depends, HTTPException, status, Response
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from firebase_admin import auth
+
+def decode_token(res: Response, cred: HTTPAuthorizationCredentials):
+    if cred is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Bearer authentication required",
+            headers={'WWW-Authenticate': 'Bearer realm="auth_required"'},
+        )
+    try:
+        token = auth.verify_id_token(cred.credentials)
+        res.headers['WWW-Authenticate'] = 'Bearer realm="auth_required"'
+        return token
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid authentication credentials. {err}",
+            headers={'WWW-Authenticate': 'Bearer error="invalid_token"'},
+        )
+
