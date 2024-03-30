e0531b0ea1bda832cbcf27798374253f04ea9721
Migas77 <miguel.belchior@ua.pt>
2024-03-30 13:23:22 +0000

    PTSE-43 update schemas

--- MessagingService/schemas.py
+++ MessagingService/schemas.py
@@ -1,49 +1,44 @@
 import json
 from enum import StrEnum
 from time import time
-from ProjectUtils.MessagingService.user_schemas import UserBase
-
-
-class BaseType(StrEnum):
-    pass
+from pydantic import BaseModel
 
 
 class BaseMessage:
-    message_type = "BaseType"
-    body = {}
-
-
-class UserType(BaseType):
-    CREATE_USER = "create_user"
-    DELETE_USER = "delete_user"
-
-
-class UserMessage(BaseMessage):
-    def __init__(self, message_type: UserType, user: UserBase):
+    def __init__(self, message_type: str, body: dict, ts: float = None):
         self.message_type = message_type
-        self.body = user.model_dump()
+        self.body = body
+        self.ts = ts if ts is not None else time()
 
 
-class PropertyType(BaseType):
-    CREATE_PROPERTY = "new_property"
-    UPDATE_PROPERTY = "update_property"
-    DELETE_PROPERTY = "delete_property"
+class MessageType(StrEnum):
+    USER_CREATE = "user_create"
+    USER_DELETE = "user_delete"
+    PROPERTY_CREATE = "property_create"
+    PROPERTY_UPDATE = "property_update"
+    PROPERTY_DELETE = "property_delete"
 
 
-class PropertyMessage(BaseMessage):
-    def __init__(self, message_type: PropertyType):
-        self.message_type = message_type
-        self.body = {}
-        # TODO : continue body
+class MessageFactory:
+    @staticmethod
+    def create_user_message(message_type: MessageType, user: BaseModel) -> BaseMessage:
+        if message_type not in [MessageType.USER_CREATE, MessageType.USER_DELETE]:
+            raise ValueError("Invalid MessageType for User")
+        return BaseMessage(message_type, user.model_dump(include={'email'}))
+
+    @staticmethod
+    def create_property(message_type: MessageType, prop: BaseModel) -> BaseMessage:
+        if message_type == MessageType.PROPERTY_DELETE:
+            return BaseMessage(message_type, prop.model_dump(include={'id'}))
+        elif message_type in [MessageType.PROPERTY_CREATE, MessageType.PROPERTY_UPDATE]:
+            return BaseMessage(message_type, prop.model_dump())
+        raise ValueError("Invalid MessageType for Property")
 
 
-def create_message(message: BaseMessage) -> str:
+def to_json(message: BaseMessage) -> str:
     return json.dumps({"type": message.message_type, "ts": time(), "body": message.body})
 
 
-"""
-Properties:
-update_property_data
-create_property
-delete_property
-"""
+def from_json(json_str: str) -> BaseMessage:
+    message = json.loads(json_str)
+    return BaseMessage(message["type"], message["body"], message["ts"])
