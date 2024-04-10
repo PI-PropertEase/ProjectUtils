acdaf737624706596705a5cb11bdc44c7c5c8d8b
notjoao1 <joao.dourado1@ua.pt>
2024-04-10 18:15:49 +0100

    RabbitMQ exchange breaking changes

--- MessagingService/queue_definitions.py
+++ MessagingService/queue_definitions.py
@@ -1,22 +1,52 @@
 import json
 import pika
 
+# 1 unica Topic Exchange, que dá para endereçar só para 1 queue ou para muitas (e todas)
+#
+#       - QUEUE "users" - ligação UserService -> PropertyService, para quando é criado um user
+#       - QUEUE "wrapper_events" - ligação wrappers -> PropertyService/CalendarService, para quando
+# queremos passar dados de wrappers para esses serviços (importar propriedades, registar uma reservation nova)
+#
+#       Outras queues: os wrappers vão ter cada um a sua propria queue, para poderem receber mensagens destinadas
+# unicamente para eles (por exemplo, quando queremos pedir ao wrapper Zooking para importar as propriedades de
+# um certo user), e vão ser eles a declarar essa queue.
+
+## Para enviar mensagens para os destinatários que queremos, usar as routing keys, por exemplo:
+
+#
+# def publish_new_user(user: UserBase):
+#     channel.basic_publish(
+#         exchange=EXCHANGE_NAME,
+#         routing_key=USER_QUEUE_ROUTING_KEY, # <------ AQUI!!
+#         body=to_json(MessageFactory.create_user_message(MessageType.USER_CREATE, user)),
+#         properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE),
+#     )
+#
+#
+
 connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 
 channel = connection.channel()
-EXCHANGE_NAME = "users"
-channel.exchange_declare(
-    exchange=EXCHANGE_NAME, exchange_type="fanout", durable=True
-)
-QUEUE_NAME = "users"
-created_users = channel.queue_declare(queue=QUEUE_NAME, durable=True)
-channel.queue_bind(queue=created_users.method.queue, exchange=EXCHANGE_NAME)
+EXCHANGE_NAME = "propertease.topic"
+channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type="topic", durable=True)
 
-w2a_channel = connection.channel()
-API_EXCHANGE = "apis"
-w2a_channel.exchange_declare(
-    exchange=API_EXCHANGE, exchange_type="fanout", durable=True
+USER_QUEUE_NAME = "users"
+USER_QUEUE_ROUTING_KEY = "users"
+created_users = channel.queue_declare(queue=USER_QUEUE_NAME, durable=True)
+channel.queue_bind(
+    queue=created_users.method.queue,
+    exchange=EXCHANGE_NAME,
+    routing_key=USER_QUEUE_ROUTING_KEY,
 )
-WRAPPER_TO_APP_QUEUE = "apis"
-apis = w2a_channel.queue_declare(queue=WRAPPER_TO_APP_QUEUE, durable=True)
-w2a_channel.queue_bind(queue=apis.method.queue, exchange=API_EXCHANGE)
+
+WRAPPER_TO_APP_QUEUE = "wrapper_events"
+WRAPPER_TO_APP_ROUTING_KEY = "wrapper_events"
+wrapper_events = channel.queue_declare(queue=WRAPPER_TO_APP_QUEUE, durable=True)
+channel.queue_bind(
+    queue=wrapper_events, exchange=EXCHANGE_NAME, routing_key=WRAPPER_TO_APP_ROUTING_KEY
+)
+
+
+# publish to this routing key if you want all wrappers to receive it
+WRAPPER_BROADCAST_ROUTING_KEY = "wrappers.all"
+WRAPPER_ZOOKING_ROUTING_KEY = "wrappers.zooking"
--- MessagingService/schemas.py
+++ MessagingService/schemas.py
@@ -14,6 +14,7 @@ class BaseMessage:
 class MessageType(StrEnum):
     USER_CREATE = "user_create"
     USER_DELETE = "user_delete"
+    PROPERTY_IMPORT = "property_import"
     PROPERTY_CREATE = "property_create"
     PROPERTY_UPDATE = "property_update"
     PROPERTY_DELETE = "property_delete"
@@ -27,27 +28,34 @@ class MessageFactory:
     def create_user_message(message_type: MessageType, user: BaseModel) -> BaseMessage:
         if message_type not in [MessageType.USER_CREATE, MessageType.USER_DELETE]:
             raise ValueError("Invalid MessageType for User")
-        return BaseMessage(message_type, user.model_dump(include={'email'}))
+        return BaseMessage(message_type, user.model_dump(include={"email"}))
 
     @staticmethod
     def create_property(message_type: MessageType, prop: BaseModel) -> BaseMessage:
         if message_type == MessageType.PROPERTY_DELETE:
-            return BaseMessage(message_type, prop.model_dump(include={'id'}))
+            return BaseMessage(message_type, prop.model_dump(include={"id"}))
         elif message_type in [MessageType.PROPERTY_CREATE, MessageType.PROPERTY_UPDATE]:
             return BaseMessage(message_type, prop.model_dump())
         raise ValueError("Invalid MessageType for Property")
-    
+
     @staticmethod
-    def create_reservation_message(message_type: MessageType, reservation: BaseModel) -> BaseMessage:
+    def create_reservation_message(
+        message_type: MessageType, reservation: BaseModel
+    ) -> BaseMessage:
         if message_type == MessageType.RESERVATION_DELETE:
-            return BaseMessage(message_type, reservation.model_dump(include={'id'}))
-        elif message_type in [MessageType.RESERVATION_CREATE, MessageType.RESERVATION_UPDATE]:
+            return BaseMessage(message_type, reservation.model_dump(include={"id"}))
+        elif message_type in [
+            MessageType.RESERVATION_CREATE,
+            MessageType.RESERVATION_UPDATE,
+        ]:
             return BaseMessage(message_type, reservation.model_dump())
         raise ValueError("Invalid MessageType for Reservation")
 
 
 def to_json(message: BaseMessage) -> str:
-    return json.dumps({"type": message.message_type, "ts": time(), "body": message.body})
+    return json.dumps(
+        {"type": message.message_type, "ts": time(), "body": message.body}
+    )
 
 
 def from_json(json_str: str) -> BaseMessage:
