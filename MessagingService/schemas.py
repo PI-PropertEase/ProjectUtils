3a3e763d04b400b2c97791490ad20d5168d51515
Bárbara Galiza <barnobregali02@gmail.com>
2024-04-11 21:15:31 +0100

    updated schemas with import properties response

--- MessagingService/schemas.py
+++ MessagingService/schemas.py
@@ -2,10 +2,11 @@ import json
 from enum import StrEnum
 from time import time
 from pydantic import BaseModel
+from typing import Union, Dict, List
 
 
 class BaseMessage:
-    def __init__(self, message_type: str, body: dict, ts: float = None):
+    def __init__(self, message_type: str, body: Union[Dict, List], ts: float = None):
         self.message_type = message_type
         self.body = body
         self.ts = ts if ts is not None else time()
@@ -15,6 +16,7 @@ class MessageType(StrEnum):
     USER_CREATE = "user_create"
     USER_DELETE = "user_delete"
     PROPERTY_IMPORT = "property_import"
+    PROPERTY_IMPORT_RESPONSE = "property_import_response"
     PROPERTY_CREATE = "property_create"
     PROPERTY_UPDATE = "property_update"
     PROPERTY_DELETE = "property_delete"
@@ -26,7 +28,10 @@ class MessageType(StrEnum):
 class MessageFactory:
     @staticmethod
     def create_user_message(message_type: MessageType, user: BaseModel) -> BaseMessage:
-        if message_type not in [MessageType.USER_CREATE, MessageType.USER_DELETE]:
+        if message_type not in [
+            MessageType.USER_CREATE, 
+            MessageType.USER_DELETE
+        ]:
             raise ValueError("Invalid MessageType for User")
         return BaseMessage(message_type, user.model_dump(include={"email"}))
 
@@ -34,14 +39,15 @@ class MessageFactory:
     def create_property(message_type: MessageType, prop: BaseModel) -> BaseMessage:
         if message_type == MessageType.PROPERTY_DELETE:
             return BaseMessage(message_type, prop.model_dump(include={"id"}))
-        elif message_type in [MessageType.PROPERTY_CREATE, MessageType.PROPERTY_UPDATE]:
+        elif message_type in [
+            MessageType.PROPERTY_CREATE, 
+            MessageType.PROPERTY_UPDATE
+        ]:
             return BaseMessage(message_type, prop.model_dump())
         raise ValueError("Invalid MessageType for Property")
 
     @staticmethod
-    def create_reservation_message(
-        message_type: MessageType, reservation: BaseModel
-    ) -> BaseMessage:
+    def create_reservation_message(message_type: MessageType, reservation: BaseModel) -> BaseMessage:
         if message_type == MessageType.RESERVATION_DELETE:
             return BaseMessage(message_type, reservation.model_dump(include={"id"}))
         elif message_type in [
@@ -54,6 +60,10 @@ class MessageFactory:
     @staticmethod
     def create_import_properties_message(user: BaseModel):
         return BaseMessage(MessageType.PROPERTY_IMPORT, user.model_dump(include={"email"}))
+    
+    @staticmethod
+    def create_import_properties_response_message(properties: list):
+        return BaseMessage(MessageType.PROPERTY_IMPORT_RESPONSE, properties)
 
 
 
