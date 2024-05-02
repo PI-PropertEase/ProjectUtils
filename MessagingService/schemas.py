de4c1dd52102738ca95d199d665e802e07cc3f1c
Migas77 <miguel.belchior@ua.pt>
2024-05-02 11:07:33 +0100

    management event propagation message

--- MessagingService/schemas.py
+++ MessagingService/schemas.py
@@ -36,7 +36,8 @@ class MessageType(StrEnum):
     # PropertyService requests reservation import from wrappers sending new id if it's a duplicated property
     RESERVATION_IMPORT_INITIAL_REQUEST = "reservation_import_initial_request"
     # CalendarService sends event propagation after management event creation or after reservation importation
-    EVENT_PROPAGATION = "event_propagation"
+    MANAGEMENT_EVENT_PROPAGATION = "event_propagation"
+    RESERVATION_PROPAGATION = "reservation_propagation"
     # Responses from PropertyService to the request
     PROPERTY_IMPORT_RESPONSE = "property_import_response"
     PROPERTY_IMPORT_DUPLICATE = "property_import_duplicate"
@@ -145,11 +146,14 @@ class MessageFactory:
         return BaseMessage(MessageType.RECOMMENDED_PRICE_RESPONSE, recommended_prices)
 
     @staticmethod
-    def create_event_propagation_message(event_internal_id: int, begin_datetime: datetime, end_datetime: datetime):
-        return BaseMessage(MessageType.EVENT_PROPAGATION, {
-            "internal_id": event_internal_id,
-            "begin_datetime": begin_datetime,
-            "end_datetime": end_datetime,
+    def create_event_propagation_message(
+            property_internal_id: int, event_internal_id: int, begin_datetime: datetime, end_datetime: datetime
+    ):
+        return BaseMessage(MessageType.MANAGEMENT_EVENT_PROPAGATION, {
+            "property_internal_id": property_internal_id,
+            "event_internal_id": event_internal_id,
+            "begin_datetime": begin_datetime.strftime("%Y-%m-%dT%H:%M:%S"),
+            "end_datetime": end_datetime.strftime("%Y-%m-%dT%H:%M:%S"),
         })
 
 def to_json(message: BaseMessage) -> str:
